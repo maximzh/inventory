c2649bf1a2a290cee46475178a24666d50958881
Salsa <vova.goncharenko@ekreative.com>
2016-03-28 14:29:08 +0300

    Fixed bugs

--- src/AppBundle/Controller/DeviceController.php
+++ src/AppBundle/Controller/DeviceController.php
@@ -3,21 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Device;
-use AppBundle\Form\DeviceType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-///**
-// * Class DeviceController
-// *
-// * @Security("has_role('ROLE_SUPER_ADMIN')")
-// *
-// * @Route("/admin/devices")
-// *
-// */
 class DeviceController extends Controller
 {
     /**
